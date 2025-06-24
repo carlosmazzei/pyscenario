51735772bce8224fe518650add188fdb2e135098
Cadu Mazzei <carlos.mazzei@gmail.com>
2025-06-24 15:19:51 -0300

    Add event loop fixture for pytest-asyncio 1

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,16 @@
 import tracemalloc
 
+import pytest
+
+
+@pytest.fixture
+def event_loop(function_event_loop):
+    """Provide event_loop fixture compatible with pytest-asyncio>=1."""
+    yield function_event_loop
+
+
+pytest_plugins = "pytest_asyncio"
+
 
 def pytest_configure(config):
     """Configure pytest-tracemalloc."""
